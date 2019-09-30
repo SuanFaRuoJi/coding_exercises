2852a350ef8c4dc8fe032d874492ff68777389d4
ddbzdnql <gdbzdnql@gmail.com>
2019-09-29 23:57:50 -0700

    123 Restructure

--- leetcode/123_best_time_to_buy_and_sell_stock_3/wsz_java.java
+++ leetcode/123_best_time_to_buy_and_sell_stock_3/wsz_java.java
@@ -3,63 +3,31 @@ public class wsz_java{
         if (prices.length <= 1){
             return 0;
         }
+        /*
+        if (k >= prices.length/2){ // to handle the edge case where k is so large that can sell whenever time sees fit.
+            int sum = 0;
+            for (int i=1; i<prices.length; i++){
+                int delta = Math.max(0, prices[i] - prices[i-1]);
+                sum += delta;
+            }
+            return sum;
+        }
+        */
         return bootstrap(prices, 2);
     }
 
     private int bootstrap(int[] prices, int times){
-        int[] front = difference(prices, true);
-        if (times == 1){
-            return find_max(front);
-        }
-        else{
-            int[] back = difference(prices, false);
-            return find_max(recursion(front, back, times-1));
-        }
-    }
-
-    private int[] recursion(int[] base, int[] iteration, int times){
-        if (times == 1){
-            int l = base.length;
-            int[] toRet = new int[l];
-            toRet[0] = iteration[0];
-            toRet[l-1] = base[l-1];
-            for (int i=1; i<l-1; i++){
-                toRet[i] = base[i-1] + iteration[i];
-            }
-            return toRet;
-        }
-        int[] result = recursion(base, iteration, times-1);
-        return recursion(base, result, 1);
-    }
-
-    private int find_max(int[] arr){
-        int max = Integer.MIN_VALUE;
-        for (int i=0; i<arr.length; i++){
-            if (arr[i] > max){
-                max = arr[i];
-            }
-        }
-        return max;
-    }
-
-    private int[] difference(int[] arr, boolean order){
-        int[] toRet = new int[arr.length];
-        int up_to = 0, so_far = 0;
-        if (order){ // front to back
-            for (int i=1; i<arr.length; i++){
-                int delta = arr[i] - arr[i-1];
-                up_to = Math.max(up_to + delta, 0);
+        int l = prices.length;
+        int[] accumulate = new int[l];
+        for (int index=0; index<times; index++){
+            int so_far = accumulate[0], up_to = accumulate[0];
+            for (int i=1; i<l; i++){
+                int delta = prices[i] - prices[i-1];
+                up_to = Math.max(accumulate[i], up_to + delta);
                 so_far = Math.max(so_far, up_to);
-                toRet[i] = so_far;
-            }
-        }else{ // back to front
-            for (int i=arr.length-2; i>=0; i--){
-                int delta = arr[i+1] - arr[i];
-                up_to = Math.max(up_to + delta, 0);
-                so_far = Math.max(so_far, up_to);
-                toRet[i] = so_far;
+                accumulate[i] = so_far;
             }
         }
-        return toRet;
+        return accumulate[l-1];
     }
 }
