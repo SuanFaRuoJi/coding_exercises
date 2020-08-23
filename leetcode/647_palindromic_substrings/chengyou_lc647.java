a6e439b90ed586256ca0942b5a928b1c5f575842
Chengyou Ju <chengyou0421@gmail.com>
2020-08-23 18:13:34 -0400

    Goldman Sachs OA

---
+++ leetcode/647_palindromic_substrings/chengyou_lc647.java
@@ -0,0 +1,13 @@
+public class chengyou_lc647 {
+    public int countSubstrings(String s) {
+        int n = s.length(), res = 0;
+        for (int i = 0; i < 2 * n - 1; i++) {
+            int left = i / 2, right = left + i % 2;
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                res += 1;
+                left -= 1; right += 1;
+            }
+        }
+        return res;
+    }
+}
