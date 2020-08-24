762e05936ed1b45e3625a45b1673b47ff1bb5206
Chengyou Ju <chengyou0421@gmail.com>
2020-08-24 11:43:25 -0400

    CJ LC992

---
+++ leetcode/992_subarrays_with_k_different_integers/chengyou_lc992.java
@@ -0,0 +1,24 @@
+public class chengyou_lc992 {
+    public int subarraysWithKDistinct(int[] A, int K) {
+        return atMostK(A, K) - atMostK(A, K - 1);
+    }
+
+    private int atMostK(int[] A, int K) {
+        int res = 0, i = 0, count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int j = 0; j < A.length; j++) {
+            if (!map.containsKey(A[j])) count += 1;
+            map.put(A[j], map.getOrDefault(A[j], 0) + 1);
+            while (count > K) {
+                map.put(A[i], map.get(A[i]) - 1);
+                if (map.get(A[i]) == 0) {
+                    count -= 1;
+                    map.remove(A[i]);
+                }
+                i += 1;
+            }
+            res += j - i + 1;
+        }
+        return res;
+    }
+}
