99f29246f37b30cc050998e16c45f13a1065362b
Chengyou Ju <chengyou0421@gmail.com>
2020-08-26 20:11:12 -0400

    CJ LC104

---
+++ leetcode/104_maximum_depth_of_binary_tree/chengyou_lc104_bfs.java
@@ -0,0 +1,18 @@
+public class chengyou_lc104_bfs {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int res = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                if (curr.left != null) queue.offer(curr.left);
+                if (curr.right != null) queue.offer(curr.right);
+            }
+            res += 1;
+        }
+        return res;
+    }
+}
---
+++ leetcode/104_maximum_depth_of_binary_tree/chengyou_lc104_recursion.java
@@ -0,0 +1,6 @@
+public class chengyou_lc104_recursion {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        else return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
