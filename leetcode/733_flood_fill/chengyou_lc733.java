5cd035cfb8d8a328d407a18b634fec00fea07d70
Chengyou Ju <chengyou0421@gmail.com>
2020-08-27 17:14:17 -0400

    CJ LC733

---
+++ leetcode/733_flood_fill/chengyou_lc733.java
@@ -0,0 +1,17 @@
+public class chengyou_lc733 {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) dfs(image, sr, sc, color, newColor);
+        return image;
+    }
+
+    private void dfs(int[][] image, int r, int c, int color, int newColor) {
+        if (image[r][c] == color) {
+            image[r][c] = newColor;
+            if (r - 1 >= 0) dfs(image, r - 1, c, color, newColor);
+            if (c - 1 >= 0) dfs(image, r, c - 1, color, newColor);
+            if (r + 1 < image.length) dfs(image, r + 1, c, color, newColor);
+            if (c + 1 < image[0].length) dfs(image, r, c + 1, color, newColor);
+        }
+    }
+}
