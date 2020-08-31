b3234406bd6fb83fe302d0fab089841ff5073640
Chengyou Ju <chengyou0421@gmail.com>
2020-08-31 19:33:41 -0400

    Queue + Stack, BFS

---
+++ leetcode/103_binary_tree_zigzag_level_order_traversal/jcy_lc103.java
@@ -0,0 +1,32 @@
+public class jcy_lc103 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        queue.add(root);
+        boolean levelIsOdd = false;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> curLevel = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                TreeNode nodeToAdd = null;
+                if (levelIsOdd) nodeToAdd = stack.pop();
+                else nodeToAdd = new TreeNode(curr.val);
+                curLevel.add(nodeToAdd.val);
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                    if (!levelIsOdd) stack.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                    if (!levelIsOdd) stack.add(curr.right);
+                }
+            }
+            res.add(curLevel);
+            levelIsOdd = !levelIsOdd;
+        }
+        return res;
+    }
+}
