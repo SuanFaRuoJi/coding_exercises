21501819c64f2e27953bc62b37cc80d10f72a8c0
Chengyou Ju <chengyou0421@gmail.com>
2020-09-04 17:53:27 -0400

    O(n) space

---
+++ leetcode/99_recover_binary_search_tree/jcy_lc99.java
@@ -0,0 +1,38 @@
+public class jcy_lc99 {
+    public void recoverTree(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorderTraversal(root, res);
+        int[] swapped = findSwappedElements(res);
+        recover(root, swapped[0], swapped[1], 0);
+    }
+
+    private void inorderTraversal(TreeNode node, List<Integer> res) {
+        if (node == null) return;
+        inorderTraversal(node.left, res);
+        res.add(node.val);
+        inorderTraversal(node.right, res);
+    }
+
+    private int[] findSwappedElements(List<Integer> res) {
+        int n = res.size(), first = -1, second = -1;
+        for (int i = 1; i < n; i++) {
+            if (res.get(i) < res.get(i - 1)) {
+                second = i;
+                if (first == -1) first = i - 1;
+                else break;
+            }
+        }
+        return new int[]{res.get(first), res.get(second)};
+    }
+
+    private void recover(TreeNode node, int val1, int val2, int numOfSwap) {
+        if (node == null) return;
+        if (node.val == val1 || node.val == val2) {
+            node.val = node.val == val1 ? val2 : val1;
+            numOfSwap += 1;
+            if (numOfSwap == 2) return;
+        }
+        recover(node.left, val1, val2, numOfSwap);
+        recover(node.right, val1, val2, numOfSwap);
+    }
+}
