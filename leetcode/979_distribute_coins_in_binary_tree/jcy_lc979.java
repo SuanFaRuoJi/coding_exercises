7d84298afe096790b6ed854a4504d9b35ec63aa3
Chengyou Ju <chengyou0421@gmail.com>
2020-09-04 01:41:06 -0400

    Interesting

---
+++ leetcode/979_distribute_coins_in_binary_tree/jcy_lc979.java
@@ -0,0 +1,15 @@
+public class jcy_lc979 {
+    int res = 0;
+
+    public int distributeCoins(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int left = dfs(root.left), right = dfs(root.right);
+        res += Math.abs(left) + Math.abs(right);
+        return root.val + left + right - 1;
+    }
+}
