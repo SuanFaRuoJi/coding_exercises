4b6b7ee4bd401450a04e90828da42e850dba2860
Chengyou Ju <chengyou0421@gmail.com>
2020-09-05 00:37:25 -0400

    easy problem

---
+++ leetcode/108_convert_sorted_array_to_binary_search_tree/jcy_lc108.java
@@ -0,0 +1,14 @@
+public class jcy_lc108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode helper(int[] nums, int start, int end) {
+        if (start > end) return null;
+        int mid = start + (end - start) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, start, mid - 1);
+        root.right = helper(nums, mid + 1, end);
+        return root;
+    }
+}
