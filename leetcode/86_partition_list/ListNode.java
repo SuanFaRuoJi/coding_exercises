5c4b8291493b1b285bda497892aaeb9e84251aef
Chengyou Ju <chengyou0421@gmail.com>
2020-09-06 17:19:42 -0400

    add class definition

---
+++ leetcode/86_partition_list/ListNode.java
@@ -0,0 +1,25 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(0), dummy2 = new ListNode(0);
+        ListNode p1 = dummy1, p2 = dummy2;
+        while (head != null) {
+            if (head.val < x) {
+                p1.next = head;
+                p1 = p1.next;
+            } else {
+                p2.next = head;
+                p2 = p2.next;
+            }
+            head = head.next;
+        }
+        p2.next = null;
+        p1.next = dummy2.next;
+        return dummy1.next;
+    }
+}
