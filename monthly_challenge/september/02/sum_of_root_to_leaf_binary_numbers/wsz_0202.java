ac252311c9551eb6b373bdad348f384258ade0a3
ddbzdnql <gdbzdnql@gmail.com>
2020-09-08 23:09:27 -0700

    924 and monthly challenge

--- monthly_challenge/september/02/sum_of_root_to_leaf_binary_numbers/wsz_0202.java
+++ monthly_challenge/september/02/sum_of_root_to_leaf_binary_numbers/wsz_0202.java
@@ -1,5 +1,29 @@
+import java.util.HashMap;
+import java.util.Stack;
+
 public class wsz_0202 {
     public int sumRootToLeaf(TreeNode root) {
+        Stack<TreeNode> q = new Stack<>();
+        q.push(root);
 
+        int sum = 0;
+        while (!q.isEmpty()) {
+            TreeNode cur = q.pop();
+            int cur_val = cur.val;
+            if (cur.left == null && cur.right == null) {
+                sum += cur_val;
+            } else {
+                if (cur.left != null) {
+                    int left_val = cur_val * 2 + cur.left.val;
+                    cur.left.val = left_val;
+                    q.push(cur.left);
+                }
+                if (cur.right != null) {
+                    cur.right.val = cur_val * 2 + cur.right.val;
+                    q.push(cur.right);
+                }
+            }
+        }
+        return sum;
     }
 }
