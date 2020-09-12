5c716c969ccbfad1cd2cbd23b099732c5b5a615f
Chengyou Ju <chengyou0421@gmail.com>
2020-09-12 18:53:55 -0400

    recursive

---
+++ leetcode/332_reconstruct_itinerary/jcy_lc332_2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class jcy_lc332_2 {
+    List<String> res = new ArrayList<>();
+    Map<String, PriorityQueue<String>> map = new HashMap<>();
+
+    public List<String> findItinerary(List<List<String>> tickets) {
+        for (List<String> ticket : tickets) {
+            map.putIfAbsent(ticket.get(0), new PriorityQueue<>());
+            map.get(ticket.get(0)).add(ticket.get(1));
+        }
+        helper("JFK");
+        return res;
+    }
+
+    private void helper(String s) {
+        PriorityQueue<String> pq = map.get(s);
+        while (pq != null && !pq.isEmpty()) helper(pq.poll());
+        res.add(0, s);
+    }
+}
