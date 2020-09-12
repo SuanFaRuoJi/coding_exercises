b1d4ced592a56a8adc57b110f34bace0a9ad9ba4
Chengyou Ju <chengyou0421@gmail.com>
2020-09-12 17:55:06 -0400

    predecessor, successor, recursive

---
+++ leetcode/450_delete_node_in_a_bst/jcy_lc450.java
@@ -0,0 +1,30 @@
+public class jcy_lc450 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return root;
+        if (key < root.val) root.left = deleteNode(root.left, key);
+        else if (key > root.val) root.right = deleteNode(root.right, key);
+        else {
+            if (root.left == null && root.right == null) root = null;
+            else if (root.right != null) {
+                root.val = successor(root);
+                root.right = deleteNode(root.right, root.val);
+            } else {
+                root.val = predecessor(root);
+                root.left = deleteNode(root.left, root.val);
+            }
+        }
+        return root;
+    }
+
+    private int successor(TreeNode root) {
+        root = root.right;
+        while (root.left != null) root = root.left;
+        return root.val;
+    }
+
+    private int predecessor(TreeNode root) {
+        root = root.left;
+        while (root.right != null) root = root.right;
+        return root.val;
+    }
+}
