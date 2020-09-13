8012efb6644d309c24cfbbdcc5f5512a6ed329f4
Chengyou Ju <chengyou0421@gmail.com>
2020-09-13 19:56:56 -0400

    easy, logical

---
+++ leetcode/463_island_perimeter/jcy_lc463.java
@@ -0,0 +1,17 @@
+public class jcy_lc463 {
+    public int islandPerimeter(int[][] grid) {
+        int m = grid.length, n = grid[0].length, res = 0, extra = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    res += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) extra += 1;
+                    if (i < m - 1 && grid[i + 1][j] == 1) extra += 1;
+                    if (j > 0 && grid[i][j - 1] == 1) extra += 1;
+                    if (j < n - 1 && grid[i][j + 1] == 1) extra += 1;
+                }
+            }
+        }
+        return res - extra;
+    }
+}
