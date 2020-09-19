f332afcca36728899034d68c71ea0b50ba51d752
Chengyou Ju <chengyou0421@gmail.com>
2020-09-19 16:59:14 -0400

    count frequency of two numbers

---
+++ leetcode/923_3sum_with_multiplicity/jcy_lc923_2.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class jcy_lc923_2 {
+    public int threeSumMulti(int[] A, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int res = 0, mod = 1000000007;
+        for (int i = 0; i < A.length; i++) {
+            res = (res + map.getOrDefault(target - A[i], 0)) % mod;
+            for (int j = 0; j < i; j++) {
+                int temp = A[i] + A[j];
+                map.put(temp, map.getOrDefault(temp, 0) + 1);
+            }
+        }
+        return res;
+    }
+}
