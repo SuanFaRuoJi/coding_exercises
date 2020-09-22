f3a6bf4475a711933211b21aa6782a9d2af1c492
Chengyou Ju <chengyou0421@gmail.com>
2020-09-22 16:46:50 -0400

    BFS

---
+++ leetcode/339_nested_list_weight_sum/jcy_lc339.java
@@ -0,0 +1,20 @@
+public class jcy_lc339 {
+    public int depthSum(List<NestedInteger> nestedList) {
+        Queue<NestedInteger> queue = new LinkedList<>();
+        for (NestedInteger n : nestedList) queue.add(n);
+        int depth = 1, res = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                NestedInteger curr = queue.poll();
+                if (curr.isInteger())
+                    res += curr.getInteger() * depth;
+                else {
+                    for (NestedInteger n : curr.getList())
+                        queue.add(n);
+                }
+            }
+            depth += 1;
+        }
+        return res;
+}
