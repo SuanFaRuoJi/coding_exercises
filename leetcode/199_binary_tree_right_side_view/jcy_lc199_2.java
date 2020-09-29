1607a6e076a8bf8f8b1c4238ccb192a570817047
Chengyou Ju <chengyou0421@gmail.com>
2020-09-29 17:33:21 -0400

    dfs

---
+++ leetcode/199_binary_tree_right_side_view/jcy_lc199_2.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class jcy_lc199_2 {
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return res;
+        helper(root, 0);
+        return res;
+    }
+
+    private void helper(TreeNode node, int level) {
+        if (level == res.size()) res.add(node.val);
+        if (node.right != null) helper(node.right, level + 1);
+        if (node.left != null) helper(node.left, level + 1);
+    }
+}
