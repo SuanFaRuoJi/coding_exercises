2bc4175fb600982b465d26454e5248b03b1053c3
Chengyou Ju <chengyou0421@gmail.com>
2020-09-29 17:12:14 -0400

    recursive

---
+++ leetcode/156_binary_tree_upside_down/jcy_lc156.java
@@ -0,0 +1,11 @@
+public class jcy_lc156 {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if (root == null || root.left == null) return root;
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
+}
