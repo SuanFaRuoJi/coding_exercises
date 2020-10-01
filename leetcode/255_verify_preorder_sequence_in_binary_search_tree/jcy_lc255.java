cacff384aa2e7aab8ac5d64b2017145bb7d44dd5
Chengyou Ju <chengyou0421@gmail.com>
2020-10-01 12:38:27 -0400

    using stack

---
+++ leetcode/255_verify_preorder_sequence_in_binary_search_tree/jcy_lc255.java
@@ -0,0 +1,13 @@
+public class jcy_lc255 {
+    public boolean verifyPreorder(int[] preorder) {
+        int lowBound = Integer.MIN_VALUE;
+        Stack<Integer> stack = new Stack<>();
+        for (int num : preorder) {
+            if (num < lowBound) return false;
+            while (!stack.empty() && num > stack.peek())
+                lowBound = stack.pop();
+            stack.push(num);
+        }
+        return true;
+    }
+}
