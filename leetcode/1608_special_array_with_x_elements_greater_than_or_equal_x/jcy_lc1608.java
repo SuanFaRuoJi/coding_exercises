60bb858fcc345a25d73843fe74cd30f9497f16d5
Chengyou Ju <chengyou0421@gmail.com>
2020-10-07 21:45:40 -0400

    Not that easy

---
+++ leetcode/1608_special_array_with_x_elements_greater_than_or_equal_x/jcy_lc1608.java
@@ -0,0 +1,12 @@
+public class jcy_lc1608 {
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int x = n - i;
+            if (nums[i] >= x && (i == 0 || nums[i - 1] < x))
+                return x;
+        }
+        return -1;
+    }
+}
