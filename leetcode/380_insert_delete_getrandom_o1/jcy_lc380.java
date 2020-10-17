9ebda579aad707c1391843605d9d6d2dcd4aaed6
Chengyou Ju <chengyou0421@gmail.com>
2020-10-17 17:22:53 -0400

    design

---
+++ leetcode/380_insert_delete_getrandom_o1/jcy_lc380.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class jcy_lc380 {
+    Map<Integer, Integer> map;
+    List<Integer> list;
+    Random rand = new Random();
+
+
+    /** Initialize your data structure here. */
+    public jcy_lc380() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if (map.containsKey(val)) return false;
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) return false;
+        int last = list.get(list.size() - 1);
+        int idx = map.get(val);
+        list.set(idx, last);
+        map.put(last, idx);
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true;
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return list.get(rand.nextInt(list.size()));
+    }
+}
