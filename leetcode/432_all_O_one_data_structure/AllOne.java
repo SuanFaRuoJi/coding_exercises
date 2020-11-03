d61ec17d63a9d2c76c603c599b9ad3a76d8808e3
ddbzdnql <gdbzdnql@gmail.com>
2020-11-03 01:58:41 -0800

    AllOne DS

--- leetcode/432_all_O_one_data_structure/AllOne.java
+++ leetcode/432_all_O_one_data_structure/AllOne.java
@@ -1,29 +1,13 @@
-import java.util.HashMap;
-import java.util.HashSet;
-
-public class AllOne {
-    LinkedList head, tail;
-    HashMap<String, LinkedList> reference;
-
-    class LinkedList {
-        LinkedList next, prev;
-        int val, count;
-    }
+class AllOne {
 
     /** Initialize your data structure here. */
     public AllOne() {
-        head = null;
-        tail = null;
-        reference = new HashMap<>();
+
     }
 
     /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
     public void inc(String key) {
-        if (reference.containsKey(key)) {
 
-        } else {
-
-        }
     }
 
     /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
@@ -33,56 +17,20 @@ public class AllOne {
 
     /** Returns one of the keys with maximal value. */
     public String getMaxKey() {
-        return null;
+
     }
 
     /** Returns one of the keys with Minimal value. */
     public String getMinKey() {
-        return null;
-    }
 
-    private void insert(LinkedList pos, int val) {
-        if (pos.next == null) {
-            LinkedList to_insert = new LinkedList();
-            to_insert.val = val;
-            to_insert.count = 1;
-            to_insert.prev = pos;
-            to_insert.next = null;
-            pos.next = to_insert;
-            tail = to_insert;
-            if (head == null) {
-                head = to_insert;
-            }
-        } else {
-            if (pos.next.val == val) {
-                pos.next.count ++;
-            } else {
-                LinkedList to_insert = new LinkedList();
-                to_insert.val = val;
-                to_insert.count = 1;
-                to_insert.prev = pos;
-                to_insert.next = pos.next;
-                pos.next = to_insert;
-            }
-        }
-    }
-
-    private void delete(LinkedList pos) {
-
-        if (pos == head) {
-            if (pos.next != null) {
-                pos.next.prev = null;
-            }
-            head = pos.next;
-        }
-        if (pos == tail) {
-            if (pos.prev != null) {
-                pos.prev.next = null;
-            }
-            tail = null;
-        }
-        LinkedList next = pos.next;
-        pos.prev.next = next;
-        next.prev = pos.prev;
     }
 }
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne obj = new AllOne();
+ * obj.inc(key);
+ * obj.dec(key);
+ * String param_3 = obj.getMaxKey();
+ * String param_4 = obj.getMinKey();
+ */
