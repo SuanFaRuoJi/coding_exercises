e767da31c026bc356d72b6d3f45a191ed0174fcb
ddbzdnql <gdbzdnql@gmail.com>
2020-11-08 04:44:45 -0800

    Added code

--- leetcode/341_flatten_nested_list_iterator/wsz_341.java
+++ leetcode/341_flatten_nested_list_iterator/wsz_341.java
@@ -1,18 +1,48 @@
 public class NestedIterator implements Iterator<Integer> {
-	private NestedInteger cur;
+	private Iterator<NestedInteger> cur = null;
+	private Integer cur_int = null;
+	private NestedIterator cur_nest = null;
 
     public NestedIterator(List<NestedInteger> nestedList) {
-        cur = null;
-
-    }
+		cur = nestedList.iterator();
+		next_working();
+	}
 
     @Override
     public Integer next() {
-        
-    }
+		Integer result = null;
+		if (cur_int != null) {
+			result = cur_int;
+			next_working();
+		} else {
+			result = cur_nest.next();
+			if (!cur_nest.hasNext()) {
+				next_working();
+			}
+		}
+
+		return result;
+	}
 
     @Override
     public boolean hasNext() {
-        
-    }
+		return cur_int != null || cur_nest != null;
+	}
+
+	private void NestedIterator next_working() {
+		cur_int = null;
+		cur_nest = null;
+		while (cur.hasNext()) {
+			NestedInteger next = cur.next();
+			if (next.isInteger()) {
+				cur_int = next.getInteger();
+				break;
+			}
+			NestedIterator potential = new NestedIterator();
+			if (potential.hasNext()) {
+				cur_nest = potential;
+				break;
+			}
+		}
+	}
 }
