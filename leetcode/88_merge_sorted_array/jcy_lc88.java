259efd0008a1ce85e5110e2ac64990aba8027433
Chengyou Ju <chengyou0421@gmail.com>
2020-11-10 17:38:45 -0500

    Start from the end

---
+++ leetcode/88_merge_sorted_array/jcy_lc88.java
@@ -0,0 +1,10 @@
+public class jcy_lc88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1, p2 = n - 1;
+        int p = m + n - 1;
+        while (p1 >= 0 && p2 >= 0)
+            nums1[p--] = nums1[p1] < nums2[p2] ? nums2[p2--] : nums1[p1--];
+        while (p2 >= 0)
+            nums1[p--] = nums2[p2--];
+    }
+}
