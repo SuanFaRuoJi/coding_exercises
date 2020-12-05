184f99fef7fce64789b1e672ceaeec95551f43a5
Chengyou Ju <chengyou0421@gmail.com>
2020-12-05 16:17:01 -0500

    rotate image

---
+++ leetcode/48_rotate_image/jcy_lc48.java
@@ -0,0 +1,14 @@
+public class jcy_lc48 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+}
