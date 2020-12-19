f63cd4c2cceacabcdd1c24fcb2098dffb9f763d4
Chengyou Ju <chengyou0421@gmail.com>
2020-12-18 19:52:23 -0500

    DFS + Set

---
+++ leetcode/1676_lowest_common_ancestor_of_a_binary_tree_4/jcy_lc1676.java
@@ -0,0 +1,16 @@
+public class jcy_lc1676 {
+    Set<Integer> set = new HashSet<>();
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode[] nodes) {
+        for (TreeNode node : nodes)
+            set.add(node.val);
+        return helper(root);
+    }
+
+    private TreeNode helper(TreeNode root) {
+        if (root == null || set.contains(root.val)) return root;
+        TreeNode left = helper(root.left), right = helper(root.right);
+        if (left != null && right != null) return root;
+        return left != null ? left : right;
+    }
+}
