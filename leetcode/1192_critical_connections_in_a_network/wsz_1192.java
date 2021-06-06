ed7d7b974c5fdd6a9544f5cb0f4a8753999bc054
ddbzdnql <gdbzdnql@gmail.com>
2021-06-05 22:59:36 -0700

    New critical connection

--- leetcode/1192_critical_connections_in_a_network/wsz_1192.java
+++ leetcode/1192_critical_connections_in_a_network/wsz_1192.java
@@ -2,83 +2,110 @@ import java.util.*;
 
 public class wsz_1192 {
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        int[] visited_nodes = new int[n];
         int[] uptree = new int[n];
-        uptree[0] = 1;
-
-        Stack<int[]> next = new Stack<>(), history = new Stack<>();
-
-        List<List<Integer>> result = new ArrayList<>();
-
-        HashSet<Integer> visited = new HashSet<>();
-        HashSet<Integer> un_critical = new HashSet<>();
+        Arrays.fill(uptree, 0, n, -1);
 
         List<Integer>[] edges = new List[n];
-        for (List<Integer> cur : connections) {
-            if (edges[cur.get(0)] == null) {
-                edges[cur.get(0)] = new ArrayList<>();
+        Set<Integer> visited_edges = new HashSet<>();
+
+        for (int i=0; i<connections.size(); i++) {
+            int from = connections.get(i).get(0), to = connections.get(i).get(1);
+
+            if (edges[from] == null) {
+                edges[from] = new ArrayList<>();
             }
-            edges[cur.get(0)].add(cur.get(1));
-            if (edges[cur.get(1)] == null) {
-                edges[cur.get(1)] = new ArrayList<>();
+            edges[from].add(to);
+            if (edges[to] == null) {
+                edges[to] = new ArrayList<>();
             }
-            edges[cur.get(1)].add(cur.get(0));
+            edges[to].add(from);
         }
 
-        for (int cur : edges[0]) {
-            int[] to_add = {0, cur};
-            next.push(to_add);
-        }
+        int[][] hist = new int[n+1][2];
+        int hist_length = 0;
 
-        while (next.size() != 0) {
-            int[] cur = next.pop(); // {a, b} means this iteration we go from a -> b
-            if (visited.contains(hash(cur))) {
+        Stack<int[]> stack = new Stack();
+
+        stack.push(new int[]{-1, 0});
+
+        List<List<Integer>> result = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            int[] top_el = stack.peek();
+            int cur = top_el[1], prev = top_el[0];
+            // System.out.println(prev + " " + cur);
+            stack.pop();
+
+            if (prev != -1 && visited_edges.contains(hash(prev, cur, n))) {
                 continue;
             }
-            visited.add(hash(cur));
-            //System.out.println(cur[0] + "->" + cur[1]);
+            visited_edges.add(hash(prev, cur, n));
 
-            int dest = cur[1], prev = cur[0];
-            while (!history.isEmpty() && history.peek()[1] != prev) { // trim the dead end
-                history.pop();
+            // trim the dead ends;
+            while (hist_length > 0 && find(uptree, prev) != find(uptree, hist[hist_length-1][1])) {
+                // System.out.println("critical: " + hist[hist_length-1][0] + " " + hist[hist_length-1][1]);
+                List<Integer> to_add = new ArrayList<>();
+                to_add.add(hist[hist_length-1][1]);
+                to_add.add(hist[hist_length-1][0]);
+                result.add(to_add);
+                hist_length --;
             }
 
-            history.push(cur);
-
-            int identifier = uptree[dest];
-            if (identifier != 0) { // a cycle
-                while (!history.isEmpty()) {
-                    int[] to_add = history.pop();
-                    un_critical.add(hash(to_add));
-                    if (to_add[0] == dest) {
+            // clear out related history
+            if (visited_nodes[cur] != 0) {
+                int top = find(uptree, cur);
+                while (hist_length > 0 && visited_nodes[hist[hist_length-1][0]] != 0) {
+                    if (cur == hist[hist_length-1][1]) {
                         break;
                     }
+                    merge(uptree, hist[hist_length-1][1], top);
+                    // System.out.println("redundant: " + hist[hist_length-1][0] + " " + hist[hist_length-1][1]);
+                    hist_length --;
                 }
+                continue;
             }
 
-            uptree[dest] = 1;
+            if (prev != -1) {
+                hist[hist_length++] = top_el;
+            }
+            visited_nodes[cur] = 1;
 
-            for (int cur_next : edges[dest]) {
-                int[] to_add = {dest, cur_next};
-                if (!visited.contains(hash(to_add))) {
-                    next.push(to_add);
+            for (int next : edges[cur]) {
+                if (!visited_edges.contains(hash(cur, next, n))) {
+                    stack.push(new int[]{cur, next});
                 }
             }
         }
 
-        for (List<Integer> cur : connections) {
-            if (!un_critical.contains(hash(new int[] {cur.get(0), cur.get(1)}))) {
-                result.add(cur);
-            }
+        for (int i=0; i<hist_length; i++) {
+            System.out.println("critical: " + hist[i][0] + " " + hist[i][1]);
+            List<Integer> to_add = new ArrayList<>();
+            to_add.add(hist[i][1]);
+            to_add.add(hist[i][0]);
+            result.add(to_add);
         }
 
         return result;
     }
 
-    private int hash(int[] edge) {
-        if (edge[0] < edge[1]) {
-            return 10000 * edge[0] + edge[1];
-        } else {
-            return hash(new int[]{edge[1], edge[0]});
+    private static int hash(int from, int to, int n) {
+        return from > to ? (from*(n+1)+to) : (to*(n+1)+from);
+    }
+
+    private static int find(int[] uptree, int index) {
+        if (uptree[index] == -1) {
+            return index;
+        }
+
+        int result = find(uptree, uptree[index]);
+        uptree[index] = result;
+        return result;
+    }
+
+    private static void merge(int[] uptree, int from, int to) {
+        int from_top = find(uptree, from), to_top = find(uptree, to);
+        if (from_top != to_top) {
+            uptree[from_top] = to_top;
         }
     }
 }
